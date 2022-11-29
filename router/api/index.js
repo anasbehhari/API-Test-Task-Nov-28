a34d2f158adf733c09f1f0d610105f5d747ce800
Anas <an.behhari@gmail.com>
2022-11-29 22:24:05 +0100

    update

--- router/api/index.js
+++ router/api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Sector = require("../../models/Sector");
+const User = require("../../models/User");
 
 const router = express.Router();
 
@@ -15,17 +16,29 @@ router.get("/sectors", (req, res) => {
       });
     });
 });
+router.get("/users", (req, res) => {
+  User.find()
+    .then((Users) => {
+      res.send(Users);
+    })
+    .catch((error) => {
+      res.send({
+        success: false,
+        error,
+      });
+    });
+});
 router.post("/sectors/add", (req, res) => {
   const { body } = req;
-  const { name} = body;
+  const { name } = body;
   const newSector = new Sector({
-    name
+    name,
   });
   newSector
     .save()
     .then((result) => {
       if (result != null) {
-        res.send({ success: true,newSector:result });
+        res.send({ success: true, newSector: result });
       } else {
         res.send({ success: false });
       }
@@ -34,7 +47,7 @@ router.post("/sectors/add", (req, res) => {
       res.send({ success: false });
     });
 });
-router.post("/user", (req, res) => {
+router.post("/user/add", (req, res) => {
   const { body } = req;
   const { name, sectors, terms } = body;
   const newUser = new User({
@@ -46,14 +59,32 @@ router.post("/user", (req, res) => {
     .save()
     .then((result) => {
       if (result != null) {
-        res.send({ success: true });
+        res.send({ success: true, _id: result._id });
       } else {
+        console.log("gete");
         res.send({ success: false });
       }
     })
     .catch((err) => {
-      res.send({ success: false });
+      res.send({ success: false, err });
     });
+});
+
+router.post("/user/check", (req, res) => {
+  const { _id } = req.body;
+  if (_id) {
+    User.findById(_id)
+    .then((result) => {
+        if (result) {
+          res.send({ success: true });
+        } else {
+          res.send({ success: false , err: "no such user"});
+        }
+    })
+    .catch(err => {
+        res.status(501).send({success:false,err})
+    })
+  }
 });
 
 module.exports = router;
