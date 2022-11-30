5b37b3b23ec0f64ae15d6fb68c49e7d72bbce079
Anas <an.behhari@gmail.com>
2022-11-30 12:59:31 +0100

    update

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const Sector = require("./models/Sector");
 const User = require("./models/User");
 
-
 const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
@@ -20,9 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //db config
-app.get("/",(req,res)=> {
-    res.send("welcome 😊 !")
-})
+app.get("/", (req, res) => {
+  res.send("welcome 😊 !");
+});
 app.get("/api/sectors", (req, res) => {
   Sector.find()
     .then((sectors) => {
@@ -88,29 +87,61 @@ app.post("/api/user/add", (req, res) => {
       res.send({ success: false, err });
     });
 });
+app.post("/api/user/edit", (req, res) => {
+  const { _id, name, sectors } = req.body;
+  if (_id) {
+    User.findOneAndUpdate(
+      { _id },
+      { $set: { name: name, sectors: sectors } },
+      { new: true },
+      (err, doc) => {
+        if (!err) {
+          res.send({ success: true, updated: doc });
+        } else {
+          res.send({ success: false });
+        }
+      }
+    );
+  }
+});
 app.post("/api/user/check", (req, res) => {
   const { _id } = req.body;
   if (_id) {
     User.findById(_id)
-    .then((result) => {
+      .then((result) => {
         if (result) {
           res.send({ success: true });
         } else {
-          res.send({ success: false , err: "no such user"});
+          res.send({ success: false });
         }
-    })
-    .catch(err => {
-        res.status(501).send({success:false,err})
-    })
+      })
+      .catch((err) => {
+        res.send({ success: false });
+      });
   }
 });
-
+app.post("/api/user/delete", (req, res) => {
+  const { _id } = req.body;
+  if (_id) {
+    User.deleteOne({ _id })
+      .then((result) => {
+        if (result && result.deletedCount != 0) {
+          res.send({ success: true });
+        } else {
+          res.send({ success: false, err: "no such user" });
+        }
+      })
+      .catch((err) => {
+        res.status(501).send({ success: false, err });
+      });
+  }
+});
 
 mongoose
   .connect(process.env.DBURI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
   })
   .then(() => console.log("MongoDb Connected..."))
   .catch((err) => console.log(err));
